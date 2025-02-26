5dd2b69616e98af36bafff4aca0909ed53831e0d
Matthew Lim <kmmatthew8@gmail.com>
2025-02-25 22:41:58 -0500

    Created destinationsSlice.js; Recommendations page now renders

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import routesReducer from './routesSlice';
+import destinationsReducer from './destinationsSlice';
+
+export const store = configureStore({
+  reducer: {
+    routes: routesReducer,
+    destination: destinationsReducer,
+  },
+});
