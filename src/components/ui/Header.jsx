d5a84b8c8753d9ae234d8bc88e06044f8478603e
Matthew Lim <kmmatthew8@gmail.com>
2025-02-26 00:08:42 -0500

    Moved Login button into nav bar to redirect to login page; Added hamburger toggle button inside nav bar

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -10,16 +10,17 @@ export function Header() {
     router.push(path);
   };
 
-  // Navigate back to login page
   const handleLoginNavigation = () => {
     router.push('/');
   };
 
   return (
     <div className="navbar bg-gray-800">
+      {/* Toggle button inside nav bar*/}
       <div className="navbar-start">
         <div className="drawer z-[10000]">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          {/* Toggle button outside nav bar */}
           <div className="drawer-content">
             <label htmlFor="my-drawer" className="btn btn-ghost text-white bg-sky-500">
               <svg
@@ -36,10 +37,28 @@ export function Header() {
               </svg>
             </label>
           </div>
+          {/* Nav bar */}
           <div className="drawer-side">
+            {/* Close nav bar if clicking outside */}
             <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
             <ul className="menu bg-gray-800 text-white min-h-full w-80 p-4">
-              {/* Login navigation button added as the first item */}
+              <li>
+                <label htmlFor="my-drawer" className="btn btn-ghost text-white bg-sky-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h7" />
+                  </svg>
+                </label>
+              </li>
+              {/* Nav bar page buttons below the toggle */}
               <li>
                 <button onClick={handleLoginNavigation}>
                   Login
